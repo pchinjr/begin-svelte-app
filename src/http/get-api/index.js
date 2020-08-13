b67c2eb726c442d1b146d3c2869ce77b1bfbbf0d
Paul Chin Jr <pchinjr@gmail.com>
2020-08-13 18:35:43 -0400

    Update index.js

--- src/http/get-api/index.js
+++ src/http/get-api/index.js
@@ -2,7 +2,7 @@ exports.handler = async function http (req) {
   console.log('Begin API called')
   return {
     headers: {
-      'status-code':200
+      'status-code':200,
       'content-type': 'application/json; charset=utf8',
       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
     },
