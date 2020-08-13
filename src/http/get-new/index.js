469a71ad1ef9fd95b77158d923fcd6775df149b3
Paul Chin Jr <pchinjr@gmail.com>
2020-08-13 18:48:10 -0400

    added status code

--- src/http/get-new/index.js
+++ src/http/get-new/index.js
@@ -2,6 +2,7 @@
 exports.handler = async function http (req) {
   return {
     headers: {
+      'status-code': 200,
       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0',
       'content-type': 'text/html; charset=utf8'
     },
